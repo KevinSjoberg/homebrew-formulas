89ddfbbe8679186a8048217227c109c36464ed7e
Kevin Sjöberg <kev.sjoberg@gmail.com>
2013-10-16 20:37:06 +0200

    Update m formula

--- m.rb
+++ m.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class M < Formula
   homepage 'https://github.com/KevinSjoberg/m'
-  url 'https://github.com/KevinSjoberg/m/archive/v0.0.1.tar.gz'
-  sha1 '24898e60f333c125e19800acbff5e97b1010444d'
+  url 'https://github.com/KevinSjoberg/m/archive/v0.1.1.tar.gz'
+  sha1 '1bfe55fc9ee908a3d1a6fd0e98805757287340c9'
 
   def install
     (share/'m').install "m"
